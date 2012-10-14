87f650dbcb0364dcac82a10f00164e38aafacba3
Erik Hazzard <enoex1@gmail.com>
2012-10-14 00:53:38 -0700

    Fixes typo with "tackets"

--- client/run_gunicorn.sh
+++ client/run_gunicorn.sh
@@ -7,6 +7,6 @@ WORKER_CLASS=gevent
 LOGFILE=/var/log/gunicorn/gunicorn_vasirtactics.log
 
 cd /home/erik/Code/VasirTactics/client
-source /home/erik/Code/VasirTackets/client/env/bin/activate
+source /home/erik/Code/VasirTactics/client/env/bin/activate
 
 gunicorn app:app --pid=$PID_FILE --debug --log-level=debug --workers=$WORKERS --error-logfile=$LOGFILE --bind=$BIND_ADDRESS --worker-class=$WORKER_CLASS
